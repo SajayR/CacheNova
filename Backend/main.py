bc958795df3d321fcfc3a1275b8f627340fd174f
SajayR <62949586+SajayR@users.noreply.github.com>
2024-02-03 03:48:31 +0530

    Test

--- Backend/main.py
+++ Backend/main.py
@@ -1,26 +1,31 @@
 import threading
-import togetherai
+from togetherai import omen, zed
 
 
 def chaos(prompt):
     if prompt =="" or prompt == None:
         return "Please provide a prompt"
-    
+    cleanprompt = zed.cleanprompt(prompt) #returns strin
+    subjects = zed.getsubjects(cleanprompt) #will return a list of all subjects related to prompt, might need to be semantic searched
 
-
-    cleanprompt = togetherai.cleanprompt(prompt)
-
-
-    subjects = togetherai.getsubjects(cleanprompt) #will return a list of all subjects related to prompt, might need to be semantic searched
     if len(subject) < 3:
-        subjects = subject[:3]          
+        subjects = subject[:3]         
 
-    
 
-    def run_threadchaos(cleanprompt, subjects):
-        togetherai.threadchaos(cleanprompt, subjects)
+    def run_threadchaos(cleanprompt, subject):   #sends only one of the subjects to the threadchaos function
+        omen.threadchaos(cleanprompt, subject)
 
     for subject in subjects:
         thread = threading.Thread(target=run_threadchaos, args=(cleanprompt, subject))
         thread.start()
-        return "Chaos has been initiated with the subjecewacf4gtvvCD "
+        
+    link = omen.getsubjectlink(subjects)
+    return(link)
+    #return(a link to the dynamic page where the user can only see the subjects thats needed for his prompt)
+
+
+
+if __name__=="__init__":
+    prompt=frontendselamadarchod
+    chaos(prompt)
+    
